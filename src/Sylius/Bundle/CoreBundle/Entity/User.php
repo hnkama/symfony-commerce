8b9459f0d6ea8fe67933c81bb70c75393ac7e174
Peniel <zhili850702@gmail.com>
2013-07-09 12:02:09 +0800

    relate the order

--- src/Sylius/Bundle/CoreBundle/Entity/User.php
+++ src/Sylius/Bundle/CoreBundle/Entity/User.php
@@ -12,6 +12,7 @@
 namespace Sylius\Bundle\CoreBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use FOS\UserBundle\Entity\User as BaseUser;
 use Sylius\Bundle\AddressingBundle\Model\AddressInterface;
 use DateTime;
