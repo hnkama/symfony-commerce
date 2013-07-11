eff908bf471fee756245e31bcf61a802966bc243
Peniel <zhili850702@gmail.com>
2013-07-11 17:23:38 +0800

    Alert if the user hasn't logged in

--- src/Jiwen/BookmarkBundle/Controller/BookmarkController.php
+++ src/Jiwen/BookmarkBundle/Controller/BookmarkController.php
@@ -19,6 +19,7 @@ use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
  */
 class BookmarkController extends ResourceController
 {
+
 	/**
 	 * Create new resource or just display the form.
 	 */
@@ -28,39 +29,40 @@ class BookmarkController extends ResourceController
 		$resource = $this->createNew();
 		$postData = $request->request->get('jiwen_bookmark');
 		$product_id = $postData['product'];
+		if (!$this->container->get('security.context')->isGranted('ROLE_USER')) {
+			$msg = "请登录之后添加到产品收藏夹";
+		} else {
+			if ($request->isMethod('POST')) {
+				$user = $this->get('security.context')->getToken()->getUser();
 
-		if ($request->isMethod('POST')) {
-			$user = $this->get('security.context')->getToken()->getUser();
-
-			// test if this product already bookmarked in the db
-			$repositoryBookmark = $this->getDoctrine()->getManager()->getRepository('JiwenBookmarkBundle:Bookmark');
-			$bookmark = $repositoryBookmark->findBy(array(
-				'user'=>$user->getId(),
-				'product'=>$product_id,
+				// test if this product already bookmarked in the db
+				$repositoryBookmark = $this->getDoctrine()->getManager()->getRepository('JiwenBookmarkBundle:Bookmark');
+				$bookmark = $repositoryBookmark->findBy(array(
+					'user' => $user->getId(),
+					'product' => $product_id,
 				));
 
-			$repository = $this->container->get('sylius.repository.product');
-			$product = $repository->find($product_id);
+				$repository = $this->container->get('sylius.repository.product');
+				$product = $repository->find($product_id);
 
-			if(count($bookmark) === 0) {
+				if (count($bookmark) === 0) {
 
-				$resource->setUser($user);
-				$resource->setProduct($product);
-				$resource->setCreated(new \DateTime);
+					$resource->setUser($user);
+					$resource->setProduct($product);
+					$resource->setCreated(new \DateTime);
 
-				$this->create($resource);
-				$msg = $product->getName()."添加成功！";
-			} else {
-				$msg = $product->getName()."已经加入到收藏夹！";
+					$this->create($resource);
+					$msg = $product->getName() . "添加成功！";
+				} else {
+					$msg = $product->getName() . "已经加入到收藏夹！";
+				}
 			}
-
-			$serializer = $this->get('jms_serializer');
-			$response = new Response($msg);
-
-			return $response;
-
-        }
-
 		}
+
+		$serializer = $this->get('jms_serializer');
+		$response = new Response($msg);
+
+		return $response;
+	}
 
 }
